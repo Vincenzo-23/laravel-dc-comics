b8c3347c7b93dde374f2c7836d36cead04c96430
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-29 16:48:34 +0200

    ADD: added the href to the Home and Comics anchors inside the app.blade layout view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="">Home</a>
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="">Comics</a>
+                        <a class="nav-link" href="{{ route('comics.index') }}">Comics</a>
                     </li>
                     </ul>
                 </div>
