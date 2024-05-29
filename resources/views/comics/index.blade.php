288cf5be075ceea98aa685410e2343bdeebb5737
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-05-29 16:30:05 +0200

    ADD: added content inside the comics.index view

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,51 @@
-<h1>Prova comics</h1>
+@extends('layouts.app')
+
+@section('title','Comics')
+
+@section('content')
+<main>
+    <section>
+    <div class="container">
+      <table class="table">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Description</th>
+      <th>Image</th>
+      <th>Price</th>
+      <th>Series</th>
+      <th>Sale Date</th>
+      <th>Type</th>
+      <th>Artist</th>
+      <th>Writer</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($comics as $comic)
+        <tr>
+            <td>{{ $comic->id }}</td>
+            <td>
+              <a href="">
+                  {{ $comic->title }}
+                </a>
+            </td>
+            <td>{{ $comic->description}}</td>
+            <td>
+                <img src="{{ $comic->thumb }}" height="144" width="144" alt="">
+            </td>
+            <td>{{ $comic->price }}</td>
+            <td>{{ $comic->series }}</td>
+            <td>{{ $comic->sale_date }}</td>
+            <td>{{ $comic->type }}</td>
+            <td></td>
+            <td></td>
+        </tr>
+    @endforeach
+  </tbody>
+</table>
+    </div>
+    </section>
+</main>
+
+@endsection
